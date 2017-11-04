ff7111adff412e995554ad140d26fceb7a10a5c4
mohammad-rehbar <33329355+mohammad-rehbar@users.noreply.github.com>
2017-11-04 12:42:31 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/*
+    Input : This program don't take any input
+    Outpu : This program Print a define string "Hello World !"  
+/*
+
+//header files or preprocessor directory
+#include <stdio.h>
+
+//main function : returning int
+
+int main(int argv,char ** argc){    //can take command line argument
+    printf("Hello World!");         //printf is a predefined function written in stdio.h header file , it helps to print text
+    return 0;                       // it is return 0 , nothing
+}
